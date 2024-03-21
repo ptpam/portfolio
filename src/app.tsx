4b3a59c1a40d03222d29ec53d610d2111380a340
ptpam <ermismelike@gmail.com>
2024-03-21 21:40:31 +0900

    debug

--- src/app.tsx
+++ src/app.tsx
@@ -6,13 +6,15 @@ import ProjectPage from './components/project-page.tsx';
 const MainContent = () => {
   const { route } = useRoute();
   console.log(route)
-  console.log(route.startsWith('/portfolio/project/'))
-  // Determine the content based on the route
-  if (route.startsWith('/portfolio/project/')) {
-    const projectId = parseInt(route.split('/portfolio/project/')[1], 10);
-    console.log(projectId)
-    return <ProjectPage projectId={projectId} /> 
+  // Extract the project ID from the route
+  const projectIdMatch = route.match(/\/project\/(\d+)/);
+  const projectId = projectIdMatch ? parseInt(projectIdMatch[1], 10) : null;
+
+  // Determine the content based on the presence of project ID
+  if (projectId !== null) {
+    return <ProjectPage projectId={projectId} /> ;
   }
+
 
   // Default to showing the GitProfile
   return <GitProfile config={CONFIG} />;
